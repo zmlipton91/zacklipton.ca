9a7c99a46abd926a5b3c267cbec9e36e1b63a5a7
zmlipton91 <47116677+zmlipton91@users.noreply.github.com>
2021-03-08 21:01:31 -0500

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,47 @@
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+  
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id);
+  const daysSpan = clock.querySelector('.days');
+  const hoursSpan = clock.querySelector('.hours');
+  const minutesSpan = clock.querySelector('.minutes');
+  const secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  const timeinterval = setInterval(updateClock, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initializeClock('clockdiv-1', new Date('2021-04-05'));
+  initializeClock('clockdiv-2', new Date('2021-04-15'));
+  initializeClock('clockdiv-3', new Date('2021-04-22'));
+  initializeClock('clockdiv-4', new Date('2021-04-22'));
+  initializeClock('clockdiv-5', new Date('2021-04-22'));
+});
